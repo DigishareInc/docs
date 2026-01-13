0ae9fb99d86ec722fa54b65211d79cdd40cfacb8
adil chbada <nanoadil@gmail.com>
2026-01-13 20:49:39 +0100

    chore: improve nuxt 4 setup and github actions caching

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,25 @@
 export default defineNuxtConfig({
   // @ts-ignore
-  extends: ['docus'],
-  css: ['~/assets/css/main.css'],
+  extends: ["docus"],
+  future: {
+    compatibilityVersion: 5,
+  },
+  compatibilityDate: "2026-01-13",
+  css: ["~/assets/css/main.css"],
   content: {
     // @ts-ignore
-    documentDriven: true
+    documentDriven: true,
   },
   app: {
-    baseURL: process.env.NODE_ENV === 'production' ? '/docs/' : '/'
+    baseURL: process.env.NODE_ENV === "production" ? "/" : "/",
   },
   nitro: {
-    preset: 'github_pages'
+    preset: "github_pages",
   },
   robots: {
-    robotsTxt: false
+    robotsTxt: false,
   },
   llms: {
-    domain: 'https://digishareinc.github.io/docs'
-  }
-})
+    domain: "https://digishareinc.github.io",
+  },
+});
