8b1337de750fb72b02782dc9309802b0cb3be24b
adil chbada <nanoadil@gmail.com>
2026-01-13 14:47:06 +0100

    feat: configure github pages deployment with ci workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 export default defineNuxtConfig({
   // @ts-ignore
   extends: ['docus'],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  content: {
+    // @ts-ignore
+    documentDriven: true
+  },
+  app: {
+    baseURL: process.env.NODE_ENV === 'production' ? '/docs/' : '/'
+  },
+  nitro: {
+    preset: 'github_pages'
+  },
+  robots: {
+    robotsTxt: false
+  },
+  llms: {
+    domain: 'https://digishareinc.github.io/docs'
+  }
 })
